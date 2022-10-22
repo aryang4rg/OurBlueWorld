c8374477867e5e2a3352fd480dc944dcdfeea59b
Allan Zhang <allanwzhang04@gmail.com>
2022-10-22 03:57:46 -0500

    init stuff

---
+++ frontend/App.js
@@ -0,0 +1,102 @@
+const React = require("react");
+const {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  Image,
+  TextInput,
+} = require("react-native");
+const {
+  NavigationContainer,
+  useNavigation,
+} = require("@react-navigation/native");
+const {
+  createNativeStackNavigator,
+} = require("@react-navigation/native-stack");
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.startScreen}>
+      <Text style={styles.startText}>GreenTracker</Text>
+      <View style={styles.button}>
+        <Button
+          color="black"
+          title="Start Game!"
+          onPress={() => navigation.navigate("Login")}
+        />
+      </View>
+    </View>
+  );
+}
+
+function LoginScreen({ navigation }) {
+  console.log("help");
+  return (
+  <View>
+    <View style={styles.button}>
+        <Button
+          color="black"
+          title="Start Game!"
+          onPress={() => navigation.navigate("Input")}
+        />
+      </View>
+  </View>
+  );
+}
+
+function InputScreen({ navigation }) {
+  return (
+<View>
+    <Text>input</Text>
+  </View>
+  );
+}
+
+function LeaderboardScreen({ navigation }) {
+  return (
+    <View>
+        <Text>leader</Text>
+      </View>
+      );
+}
+
+function StatScreen({ navigation }) {
+  return (
+    <View>
+        <Text>stat</Text>
+      </View>
+      );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={({ route }) => ({ title: route.params.name })}/>
+        <Stack.Screen name="Input" component={InputScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  startScreen: {
+    backgroundColor: "#e3bac6",
+    justifyContent: "top",
+    alignItems: "center",
+    flex: 1,
+  },
+  startText: {
+    fontFamily: "Cochin",
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 30,
+  },
+});
