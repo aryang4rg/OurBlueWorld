486cd449142a018ee06b797af18e2904a0677836
Evelyn <evelynzh.022@gmail.com>
2022-10-22 03:33:39 -0500

    hello

--- evelyn_test/main.py
+++ evelyn_test/main.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello World'
 
+@app.route('/hello')
+def good_morning():
+   return 'Good Morning'
+
+@app.route('/bye/<name>')
+def bye_name(name):
+   return 'Bye %s!' %name
+
+
 if __name__ == '__main__':
    app.run()
