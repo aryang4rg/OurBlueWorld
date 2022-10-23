33b1f8d8596995a75abb7df7a2df10a0ed09eb7a
Allan Zhang <allanwzhang04@gmail.com>
2022-10-22 19:52:43 -0500

    made global colors

--- frontend/util.js
+++ frontend/util.js
@@ -4,7 +4,8 @@ export const DATACONST = {
     BASEURL: "http://localhost:3000/",
     font: "Helvetica",
     fontColor: "#7fcdff",
+    fontColor2: "#045e8f",
     bkgColorMain: "#064273",
-    bkgColorSecondary: "#1da2d8",
+    bkgColorSecondary: "#def3f6",
     bkgColorThree: "#76b6c4",
 }
