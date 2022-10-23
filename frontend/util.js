a5a4ad4d142fa6b1da6231dca68c11259a5c7652
agarg <agarg.usa@gmail.com>
2022-10-22 22:19:47 -0500

    created basic login page

--- frontend/util.js
+++ frontend/util.js
@@ -2,10 +2,12 @@ const React = require("react");
 
 export const DATACONST = {
     BASEURL: "http://localhost:3000/",
+	LOGOURL: "https://www.pngkey.com/png/detail/99-992640_ocean-logo.png",
     font: "Helvetica",
     fontColor: "#7fcdff",
     fontColor2: "#045e8f",
     bkgColorMain: "#064273",
     bkgColorSecondary: "#def3f6",
     bkgColorThree: "#76b6c4",
+	bkgColorFour: "#fff1d0",
 }
